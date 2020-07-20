86015c54a17f7188d6916730b82b26dc11b1dab2
Tomas Ortiz <TomasOrtiz@protonmail.com>
2020-07-20 14:09:47 -0700

    MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,41 @@
 # TO-DO: Complete the selection_sort() function below
+# Runtime O(n^2)
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr) - 1):  # makes it so that we iterate over each item one at a time
         cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        lowest_index = cur_index
+        # we need it to evaluate each index and compare it with to the lowest value.
 
+        # iterate over each item in the list to find the lowest value
+        for j in range(cur_index, len(arr)):
 
-        # TO-DO: swap
-        # Your code here
+            # once the lowest is found, move to the left and swap the larger value in it's old place
+            if arr[j] < arr[lowest_index]:
+                lowest_index = j
 
+        # repeat swaps until there is nothing left to swap, then return the list
+        arr[lowest_index], arr[cur_index] = arr[cur_index], arr[lowest_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
+# Runtime O(n^2)
 def bubble_sort(arr):
     # Your code here
-
-
+    swapped = True  # set a boolean check for if there have been any swaps
+    while swapped:
+        swapped = False
+        # makes it so that we iterate over each item one at a time
+        for i in range(len(arr) - 1):
+            print(arr)  # without this it fails the test for whatever reason
+            # check the neigboring index and only if it's smaller you...
+            if arr[i] > arr[i + 1]:
+                #  swap indexes so the lesser value is on the left and greater is on the right and keep checking if a swap was made
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True  # once the final pass has been made and there have been no swaps, switch the bool to end the loop and return sorted
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +54,9 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,31 @@
+# Runtime O(n)
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+    for i in range(len(arr)):  # loop through all the items in the list
+        if arr[i] == target:  # if the target is found early, return it
+            return i
+    return -1  # or you can wait until the for loop finishes before returning a value or not found
+    # we return -1 as not found instead of False to avoid type errors
+    # if we're going to use a Binary search, we need to make sure the data is sorted first otherwise it won't work
 
 
 # Write an iterative implementation of Binary Search
+# Runtime: O(log(n))
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    left = 0  # low is the pointer to the first index in the list
+    right = len(arr) - 1  # high is the pointer to the last index in the list
+    while left <= right:
+        # find the midpoint and continue
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        # check to see if the element should be on the left or right side
+        # of our midpoint
+        elif arr[mid] < target:
+            # toss out the left side of the arr
+            # update our `left` index
+            left = mid + 1
+        # otherwise, arr[mid] > target
+        else:
+            # toss out the right side of the arr because the element has to be on the left side
+            right = mid - 1
     return -1  # not found
